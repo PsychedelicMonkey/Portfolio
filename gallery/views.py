89c7e11e8bb3987f5190f939ac7463a53af724c8
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-05-12 22:02:54 -0600

    About page

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home Page',
+        'title': 'Gallery',
+        'page': 'gallery',
     }
     return render(request, 'gallery/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'About Me',
+        'page': 'about',
+    }
+    return render(request, 'gallery/about.html', context)
