0ac3824c90c5fcbf4bbb92386c6637798812dd48
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-05-12 23:11:02 -0600

    Social media links

--- user/models.py
+++ user/models.py
@@ -16,3 +16,21 @@ class Profile(models.Model):
         except:
             img = ''
         return img
+
+class SocialMediaUrl(models.Model):
+    CHOICES = (
+        ('YOUTUBE', 'YouTube'),
+        ('FACEBOOK', 'Facebook'),
+        ('TWITTER', 'Twitter'),
+        ('PINTEREST', 'Pinterest'),
+        ('INSTAGRAM', 'Instagram'),
+        ('LINKEDIN', 'LinkedIn'),
+        ('GITHUB', 'GitHub'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    site = models.CharField(max_length=20, choices=CHOICES, null=True, blank=True)
+    url = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.site}'
