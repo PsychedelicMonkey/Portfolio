9de884f7b2acbb6d00a1e7f2a2768627c8adfd28
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-05-12 22:29:48 -0600

    Dynamic profile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    biography = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @property
+    def imageUrl(self):
+        try:
+            img = self.image.url
+        except:
+            img = ''
+        return img
--- user/views.py
+++ user/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def about(request):
+    user = User.objects.get(pk=1)
     context = {
         'title': 'About Me',
         'page': 'about',
+        'user': user,
     }
     return render(request, 'user/about.html', context)
 
