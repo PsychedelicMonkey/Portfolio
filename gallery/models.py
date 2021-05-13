7ca9c3b6ede371b992bfa58a5181bb1e63d6c711
Ryan <45304409+PsychedelicMonkey@users.noreply.github.com>
2021-05-12 23:25:03 -0600

    Dynamic gallery

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def imageUrl(self):
+        try:
+            img = self.image.url
+        except:
+            img = ''
+        return img
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Photo
 
-def index(request):
-    context = {
+class Gallery(ListView):
+    model = Photo
+    ordering = ['-created']
+    template_name = 'gallery/index.html'
+    context_object_name = 'photos'
+    extra_context = {
         'title': 'Gallery',
         'page': 'gallery',
     }
-    return render(request, 'gallery/index.html', context)
